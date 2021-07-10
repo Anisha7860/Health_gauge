e21a639ff892c02fca586fb9163d989f9ecef579
sayNawaf <saynawaf6399@gmail.com>
2021-07-10 12:59:54 +0530

    flask update

--- flask_websiteBackend.py
+++ flask_websiteBackend.py
@@ -2,6 +2,20 @@ from flask import Flask,request
 from flask import render_template
 
 app = Flask(__name__)
+Genderl = ['female', 'male', 'trans']
+family_historyl = ['No', 'Yes']
+treatmentl = ['No', 'Yes']
+benefitsl =  ["Don't know", 'No', 'Yes']
+care_optionsl = ['No', 'Not sure', 'Yes']
+anonymityl = ["Don't know", 'No', 'Yes']
+leavel = ["Don't know", 'Somewhat difficult', 'Somewhat easy', 'Very difficult', 'Very easy']
+work_interferel = ["Don't know", 'Never', 'Often', 'Rarely', 'Sometimes']
+coworkersl =  ['No', 'Some of them', 'Yes'] 
+
+def CheckPositionNumber(string,lists):
+    for no,data in enumerate(lists):
+        if string == data:
+            return no
 
 @app.route('/HomePage')
 def index():
@@ -13,14 +27,17 @@ def MentalHealthpage():
         return render_template("quizm.html")
     elif (request.method == "POST"):
         Age = int(request.form['Age'])
-        Gender = str(request.form['Gender'])
-        family_history = str(request.form['family_history'])
-        benefits = str(request.form['benefits'])
-        care_options = str(request.form['care_options'])
-        anonymity = str(request.form['anonymity'])
-        leave = str(request.form['leave'])
-        work_interfere = str(request.form['work_interfere'])
-        coworkers = str(request.form['coworkers'])
+        Gender = CheckPositionNumber(str(request.form['Gender']),Genderl)
+        family_history = CheckPositionNumber(str(request.form['family_history']),family_historyl)
+        benefits = CheckPositionNumber(str(request.form['benefits']),benefitsl)
+        care_options = CheckPositionNumber(str(request.form['care_options']),care_optionsl)
+        anonymity = CheckPositionNumber(str(request.form['anonymity']),anonymityl)
+        leave = CheckPositionNumber(str(request.form['leave']),leavel)
+        work_interfere = CheckPositionNumber(str(request.form['work_interfere']),work_interferel)
+        coworkers = CheckPositionNumber(str(request.form['coworkers']),coworkersl)
+       
+
+
         
 
 @app.route("/PhysicalHealth")
